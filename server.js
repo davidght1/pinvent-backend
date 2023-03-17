e2462cb6d0d41722fed1db28658124f402cb9950
David Agam <davidght1@gmail.com>
2023-03-18 01:54:59 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://pinvent-frontend-nine.vercel.app/",
-    ],
-    credentials: true,
-  })
+  cors()
+  // cors({
+  //   origin: [
+  //     "http://localhost:3000",
+  //     "https://pinvent-frontend-nine.vercel.app/",
+  //   ],
+  //   credentials: true,
+  // })
 );
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
