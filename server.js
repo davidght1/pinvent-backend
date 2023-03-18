ecf31f4bf8941b890fe2d188ade5ceb6c004b859
David Agam <davidght1@gmail.com>
2023-03-18 02:55:13 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: [
-      // "http://localhost:3000",
+      "http://localhost:3000",
       "https://pinvent-frontend-nine.vercel.app",
     ],
     credentials: true,
